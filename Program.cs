772a9bdf2f2eef165cd9a522976d43edf67d51ae
devjunaeid <junaeidthamid@gmail.com>
2025-07-23 15:25:53 +0600

    Added: Main Ui Initialization & Dep. Pkg.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DotNETConsole.DrinksInfo;
+
+using System.Threading.Tasks;
+using Controllers;
+
+
+public class Program{
+  private static MenuController _menuController = new MenuController();
+  public static async Task Main(string[] args){
+      await _menuController.MainMenu();
+  }
+}
+
