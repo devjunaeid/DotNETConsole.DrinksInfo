0faa815bae49c90e35acfe8ccb9cc1fb7e97c858
devjunaeid <junaeidthamid@gmail.com>
2025-07-23 15:26:43 +0600

    Added: ToString() option to the record.

--- Repository/DrinkCategoryRecord.cs
+++ Repository/DrinkCategoryRecord.cs
@@ -3,4 +3,9 @@ using System.Text.Json.Serialization;
 
 public record class DrinkCategoryRecord(
     [property: JsonPropertyName("strCategory")] string DrinkCategory
-){}
+){
+  public override string ToString()
+  {
+    return $"{this.DrinkCategory}";
+  }
+}
